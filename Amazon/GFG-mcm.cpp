800877c5f9603d2d517f529c156f4c60d11bf5d3
Shreya-Joshi23 <joshi2003shreya@gmail.com>
2025-01-18 11:36:43 +0530

    Matrix chain multiplication

--- Amazon/GFG-mcm.cpp
+++ Amazon/GFG-mcm.cpp
@@ -14,7 +14,46 @@ public:
                     st.insert(box);
                 }else{
                     return false;
-                }
+                } pair<string,int> solve(int i,int j,vector<int>&arr,vector<vector<pair<string,int>>>&dp){
+        if(i==j){
+            string s="";
+            s+='A'+i-1;
+            return {s,0};
+        }   
+        if(dp[i][j].second!=-1)return dp[i][j];
+        int mini=INT_MAX;
+        string sr="";
+        for(int k=i;k<j;k++){
+            string s="";
+            auto left=solve(i,k,arr,dp);
+            auto right=solve(k+1,j,arr,dp);
+            int x=left.second+right.second+arr[i-1]*arr[k]*arr[j];
+            s+='('+left.first+right.first+')';
+            if(mini>x){
+                mini=x;
+                sr=s;
+            }
+        }
+        return dp[i][j]={sr,mini};
+    }
+    
+    string matrixChainOrder(vector<int> &arr) {
+        // code here
+        /*
+            [40, 20, 30, 10, 30]
+            [40,20] [20,30] [30,10] [10,30]
+              A        B       C       D
+            [40,20]    [20,10]       [10,30]
+                 [40,10]        [10,30]
+                        [40,30]
+            (40*10*30)+(40*20*10)+(20*30*10)
+        */
+        
+        int n=arr.size();
+        vector<vector<pair<string,int>>>dp(n,vector<pair<string,int>>(n,pair<string,int>({"",-1})));
+        auto p=solve(1,n-1,arr,dp);
+        return p.first;
+    }
                 }
             }
         }
